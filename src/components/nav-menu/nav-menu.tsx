a3386558a16665ef66c8b9e6c213168b7a9a2bbc
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2024-10-09 11:04:41 +0300

    link navMenu

--- src/components/nav-menu/nav-menu.tsx
+++ src/components/nav-menu/nav-menu.tsx
@@ -1,17 +1,40 @@
-import {NavLinkMenu} from '../../const.ts';
+import {AppRoute, NavLinkMenu} from '../../const.ts';
+import {Link} from 'react-router-dom';
 
 export default function NavMenu () {
+
   return (
     <nav className="main-nav header__main-nav">
       <ul className="main-nav__list">
-        {Object.entries(NavLinkMenu).map(([item1,item2]) => (
-          <li className="main-nav__item" key={item1}>
-            <a className="main-nav__link" href="#">
-              {item2}
-            </a>
-          </li>
-        ))}
-
+        <li className="main-nav__item">
+          <Link
+            className="main-nav__link"
+            to={AppRoute.Main}
+          >
+            {NavLinkMenu.Catalog}
+          </Link>
+        </li>
+        <li className="main-nav__item">
+          <a
+            className="main-nav__link"
+          >
+            {NavLinkMenu.Guarantee}
+          </a>
+        </li>
+        <li className="main-nav__item">
+          <a
+            className="main-nav__link"
+          >
+            {NavLinkMenu.Delivery}
+          </a>
+        </li>
+        <li className="main-nav__item">
+          <a
+            className="main-nav__link"
+          >
+            {NavLinkMenu.About}
+          </a>
+        </li>
       </ul>
     </nav>
   );
--- src/store/data-card-process/data-card-process.ts
+++ src/store/data-card-process/data-card-process.ts
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {TCameraCard} from '../../types/type-cards.ts';
 import {NameSpace} from '../../const.ts';
 import {fetchCameraCardAction, fetchCameraCardsAction} from '../api-actions.ts';
@@ -7,18 +7,24 @@ export type TInitialStateDataCard = {
   cameras: TCameraCard [];
   camera: TCameraCard | null;
   isLoading: boolean;
+  currentNavTab: string;
 }
 
 const initialState: TInitialStateDataCard = {
   cameras: [],
   camera: null,
   isLoading: false,
+  currentNavTab: '',
 };
 
 export const dataCardProcess = createSlice({
   name: NameSpace.DATA_CARDS,
   initialState,
-  reducers: {},
+  reducers: {
+    setCurrentNavTab: (state, action: PayloadAction<string>) => {
+      state.currentNavTab = action.payload
+    }
+  },
   extraReducers (builder) {
     builder
       .addCase(fetchCameraCardsAction.fulfilled, (state, action) => {
@@ -36,3 +42,5 @@ export const dataCardProcess = createSlice({
       });
   }
 });
+
+export const {setCurrentNavTab} = dataCardProcess.actions
