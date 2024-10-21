f2c9b1a96755e319c3b1abe55cd364f660397d5e
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2024-10-21 13:59:38 +0300

    final fix v1.3

--- src/components/product-reviews-button-up/product-reviews-button-up.tsx
+++ src/components/product-reviews-button-up/product-reviews-button-up.tsx
@@ -1,6 +1,8 @@
 export default function ProductReviewsButtonUp() {
+  const handleClickUp = () => window.scrollTo({ top: 0, behavior: 'smooth' });
+
   return (
-    <a className="up-btn" href="#header" data-testid="product-reviews-button-up">
+    <a className="up-btn" data-testid="product-reviews-button-up" onClick={handleClickUp}>
       <svg width={12} height={18} aria-hidden="true">
         <use xlinkHref="#icon-arrow2"/>
       </svg>
