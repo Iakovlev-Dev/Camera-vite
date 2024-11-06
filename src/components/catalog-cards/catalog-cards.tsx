286043208628206dbd84ded7d05e86271462ead9
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2024-11-06 11:29:15 +0300

    filters level, type, category done fix 1.2

--- src/components/catalog-cards/catalog-cards.tsx
+++ src/components/catalog-cards/catalog-cards.tsx
@@ -1,20 +1,17 @@
 import CatalogCard from '../catalog-card/catalog-card.tsx';
-import {useAppDispatch, useAppSelector} from '../../store/hooks.ts';
+import {useAppSelector} from '../../store/hooks.ts';
 import {selectCameras} from '../../store/data-card-process/selectors.ts';
 import {selectSortInner, selectSortOrder} from '../../store/sorting-filtered-process/selectors.ts';
 import {sortingCameras} from '../../utils/utils.ts';
 import {selectFilterCategory, selectFilterLevel, selectFilterType} from '../../store/filters-process/selectors.ts';
 import {TCameraCard} from '../../types/type-cards.ts';
-import {useEffect} from 'react';
-import {setFilterPriceDown, setFilterPriceUp} from '../../store/filters-process/filter-process.ts';
+
 
 type TCatalogCard = {
   onClick: (id: number) => void;
 }
 
 export default function CatalogCards ({onClick}: TCatalogCard) {
-  const dispatch = useAppDispatch();
-
   const handleClick = (id: number) => {
     onClick(id);
   };
@@ -38,22 +35,10 @@ export default function CatalogCards ({onClick}: TCatalogCard) {
 
   const filteredCameras = sortedCameras?.filter((camera: TCameraCard) => getFilteredCameras(camera));
 
-  useEffect(() => {
-    if(filteredCameras) {
-      const priceDown = filteredCameras[0]?.price.toString();
-      const priceUp = filteredCameras[filteredCameras.length - 1]?.price.toString();
-      dispatch(setFilterPriceDown(priceDown));
-      dispatch(setFilterPriceUp(priceUp));
-    }
-  }, [filteredCameras, currentFilterType, currentFilterLevel, currentFilterCategory, dispatch]);
-
-  if (!filteredCameras) {
+  if (!filteredCameras && !sortedCameras) {
     return null;
   }
 
-  if(!sortedCameras) {
-    return null;
-  }
   return (filteredCameras &&
     <div className="cards catalog__cards" data-testid="catalog-cards">
       {filteredCameras.map((camera: TCameraCard) => (
