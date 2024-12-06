0644bd6f67d9f6618f400fae3e3868d29ad606f9
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2024-12-06 20:03:27 +0300

    add new components in basket.tsx

---
+++ src/components/basket-list/basket-list.tsx
@@ -0,0 +1,9 @@
+import BasketItemCamera from '../basket-item-camera/basket-item-camera.tsx';
+
+export default function BasketList () {
+  return (
+    <ul className="basket__list">
+      <BasketItemCamera/>
+    </ul>
+  )
+}
